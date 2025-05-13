b7911f9a79f6939eafa055ff0541463ddc6ef779
Joe Cronin <joseph.cronin10@gmail.com>
2025-05-13 20:46:35 +0200

    config.h

--- config.h
+++ config.h
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "a", "o", "e", "i", "u", "d", "h", "t", "n", "s", };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -59,42 +59,49 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+static const char *rofi_cmd[] = { "rofi", "-show", "drun" };
+static const char *browser_cmd[] = { "librewolf", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                     	XK_k,      spawn,          SHCMD("pactl set-sink-volume alsa_output.usb-Focusrite_Scarlett_Solo_USB_Y79Y5F416098C2-00.HiFi__Line1__sink -5%") },
+	{ MODKEY,                     	XK_comma,  spawn,          SHCMD("pactl set-sink-volume alsa_output.usb-Focusrite_Scarlett_Solo_USB_Y79Y5F416098C2-00.HiFi__Line1__sink +5%") },
+	{ MODKEY, 											XK_q,		 	 spawn, SHCMD ("rofi -show calc -modi calc -no-show-match -no-sort | xclip -selection clipboard") },
+	{ ControlMask,									XK_grave,  spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = rofi_cmd } },
+	{ ControlMask,                  XK_u,      spawn,          {.v = browser_cmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_comma,  focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_r,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_period, incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_g,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_semicolon,setmfact,     {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_w,			 zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY,					              XK_c,      killclient,     {0} },
+	// { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	// { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	// { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	// { MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	// { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	// { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
+	TAGKEYS(                        XK_a,                      0)
+	TAGKEYS(                        XK_o,                      1)
+	TAGKEYS(                        XK_e,                      2)
+	TAGKEYS(                        XK_i,                      3)
+	TAGKEYS(                        XK_u,                      4)
+	TAGKEYS(                        XK_d,                      5)
+	TAGKEYS(                        XK_h,                      6)
+	TAGKEYS(                        XK_t,                      7)
+	TAGKEYS(                        XK_n,                      8)
+	TAGKEYS(                        XK_s,                      9)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
